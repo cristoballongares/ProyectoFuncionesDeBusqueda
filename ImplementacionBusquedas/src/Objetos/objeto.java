a21586097caacbf23765effe37154f24a07164c1
cristoballongares <78714887+cristoballongares@users.noreply.github.com>
2025-12-07 14:17:17 -0600

    :)

---
+++ ImplementacionBusquedas/src/Objetos/objeto.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author todos:)
+ */
+public class objeto { // Cambiar nombre de la clase en cuanto lo tengamos decidido
+    
+    // 1.- Figuras gemometricas
+    // Se tiene un arreglo de figuras geometricas, en el arreglo, las figuras se almacenan en orden ascendente tomando en 
+    // cuenta el area de dicha figura.
+    // mostrar arreglo de figuras geometricas ... (desordenado :p)
+    
+    // Figura geometrica
+    // Nombre (Cuadrado, Rombo, Triangulo, etc...)
+    // Perimetro (12, 9.89, 5,...)
+    // Area (93.33, 12.4, 3.1416, 9)
+    
+    // Bri tiene un arreglo de figuras geometricas (como triangulos, cuadrados, poligono, etc), cada figura
+    // cuenta con 3 caracteristicas: nombre, perimetro y area. A Bri y a sus amigos les gustan mucho los retos que involucran
+    // un orden estricto y figuras geometricas, es por eso, que proponen una serie de desafios a sus compañeros de clase.
+    // David propone lo siguiente: Si el arreglo esta ordenado a partir del nombre de las figuras geometricas (orden lexicografico)
+    // cual y como seria la forma mas eficiente de encontrar la cantidad de apariciones de la figura con nombre "Triangulo" usando busqueda binaria?
+    
+    // EJEMPLO DE UN ARREGLO:
+    // Cuadrado, Cuadrado, Cuadrado, Triangulo, Triangulo, Rombo, Rombo, Hexagono
+    
+    // Solucion
+    // Primero: Encontrar la ultima aparicion de Triangulo usando busqueda binaria
+    // Segundo: Encontrar la primera aparicion de Triangulo usando busqueda binaria
+    
+    
+}
